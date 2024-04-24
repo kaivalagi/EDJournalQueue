dd016afdf4a26ae974e62bea0ef02b119dda10ee
kaivalagi <kaivalagi@users.noreply.github.com>
2024-04-24 14:27:10 +0100

    initial commit

---
+++ Models/JournalEntryMissionRemoved.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace EDJournalQueue.Models
+{
+    public class JournalEntryMissionRemoved : JournalEntryMissionBase
+    {
+
+        #region Constructor
+
+        public JournalEntryMissionRemoved(JToken entry) : base(entry)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
